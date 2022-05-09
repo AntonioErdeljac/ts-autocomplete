7bef66f08ae045ce66bafaf90673d3d6b87d1625
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2022-05-09 02:41:11 +0200

    refactor: separate constants

---
+++ src/constants/index.ts
@@ -0,0 +1,11 @@
+/**
+ * https://stackoverflow.com/questions/63961803/eslint-says-all-enums-in-typescript-app-are-already-declared-in-the-upper-scope)
+ */
+// eslint-disable-next-line no-shadow
+export enum KEY_MAP {
+  ENTER = 13,
+  UP = 40,
+  DOWN = 38,
+}
+
+export const MAX_RESULTS_HEIGHT = 500;
--- src/fragments/Autocomplete/index.tsx
+++ src/fragments/Autocomplete/index.tsx
@@ -12,18 +12,7 @@ import './index.css';
 
 import { Results, Input } from '../../components';
 import { getFilteredData, getLabel, getValue } from '../../utils';
-
-/**
- * Eslint no shadow bug (See: https://stackoverflow.com/questions/63961803/eslint-says-all-enums-in-typescript-app-are-already-declared-in-the-upper-scope)
- */
-// eslint-disable-next-line no-shadow
-enum KEY_MAP {
-  ENTER = 13,
-  UP = 40,
-  DOWN = 38,
-}
-
-const MAX_RESULTS_HEIGHT = 500;
+import { KEY_MAP, MAX_RESULTS_HEIGHT } from '../../constants';
 
 type Props = {
   loading?: boolean;
